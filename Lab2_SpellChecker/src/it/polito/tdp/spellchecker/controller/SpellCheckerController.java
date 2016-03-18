93ba25e8680e7c5d856946189c666a2190980b31
183346 <Robybe1@Roby>
2016-03-19 00:09:16 +0100

    finito

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,9 +1,17 @@
 package it.polito.tdp.spellchecker.controller;
 
-import java.awt.Color;
+//import java.awt.Color;
 import java.net.URL;
+import java.time.LocalDateTime;
+//import java.text.MessageFormat;
 import java.util.List;
 import java.util.ResourceBundle;
+//import javax.swing.*;
+
+
+
+import java.awt.*;
+import java.util.*;
 
 import it.polito.tdp.spellchecker.model.Dictionary;
 import it.polito.tdp.spellchecker.model.RichWord;
@@ -13,21 +21,32 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+//import javafx.scene.effect.ColorInput;
+import javafx.scene.effect.Effect;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.util.StringConverter;
+import javafx.*;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 public class SpellCheckerController {
 	
 	Dictionary model = new Dictionary();
-	Color rosso = Color.red;
-	
+
+	List<Text> testi = new LinkedList<Text>();
 	
 
-    public Color getRosso() {
-		return rosso;
-	}
-
-	public void setRosso(Color rosso) {
-		this.rosso = rosso;
-	}
+   
 
 	public void setModel(Dictionary model) {
 		   	this.model = model;
@@ -39,6 +58,11 @@ public class SpellCheckerController {
 
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
+    
+    @FXML
+    private TextArea txtOutput;
+    @FXML // fx:id="txtOutput1"
+    private TextFlow txtOutput1; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtMessage"
     private TextField txtMessage; // Value injected by FXMLLoader
@@ -57,35 +81,63 @@ public class SpellCheckerController {
 
     @FXML // fx:id="txtTime"
     private TextField txtTime; // Value injected by FXMLLoader
+    
+   
 
     @FXML
     void doSpell(ActionEvent event) {
-    	String risultatoS="";
+    	LocalDateTime now = LocalDateTime.now();
     	
-    	
+    	if(this.comboLanguage.getValue()==null){this.txtMessage.setText("select a language");return;}
+    	boolean errore=false;
+    	this.txtMessage.clear();
+    	    	
     	String lingua=this.comboLanguage.getValue().toString();
+    	this.testi.clear();
+    	this.txtOutput1.getChildren().clear();
     	
     	String testo = this.txtInput.getText();
+    	testo=testo.toLowerCase();
+    	
     	String[] vettore= testo.split("\\W");
-    	/*for(int i=0;i<vettore.length;i++){
-    	System.out.println(vettore[i]+"\n");}*/
+    	
     	List<RichWord> risultato=model.traduzione(lingua,vettore);
+    	   	
+    	
     	for(RichWord rw:risultato){
-    		if (rw.isCorretta()){
-    			risultatoS=risultatoS+rw.getParola()+" ";
+    	   		if (rw.isCorretta()){
+    			Text testoNero = new Text(rw.getParola()+" ");
+    			testi.add(testoNero);
+    			    			
     		}else{
-    			String colorato=rw.getParola();
-    			
-    			
-    			
-    			risultatoS=risultatoS+rw.getParola();
+    			errore=true;
+    			Text testoRosso = new Text(rw.getParola()+" ");
+    			testoRosso.setFill(Color.RED);
+    			testi.add(testoRosso);
     		}
     	}
+    	LocalDateTime now1 = LocalDateTime.now();
+    	Long diffe= (long) (now1.getNano()-now.getNano());
+    	
+    	
+    	this.txtOutput1.getChildren().addAll(testi);
+    	if(errore){
+    		String messaggio="Your text contains errors";
+    		this.txtMessage.setStyle("-fx-text-fill: red");
+    		this.txtMessage.setText(messaggio);
+    		
+    	}
+    	this.txtTime.setText(" il tempo di elaborazione è:   "+(double)(diffe)/(1000000000)+" secondi");
 
     }
 
     @FXML
     void doClear(ActionEvent event) {
+    	boolean errore=false;
+    	this.txtMessage.clear();
+    	this.txtInput.clear();
+    	this.txtOutput1.getChildren().clear();
+    	this.txtTime.clear();
 
     }
 
@@ -97,6 +149,7 @@ public class SpellCheckerController {
         assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert comboLanguage != null : "fx:id=\"comboLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtOutput1 != null : "fx:id=\"txtOutput1\" was not injected: check your FXML file 'SpellChecker.fxml'.";
 
     }
 }
